595b679a4a7b8d4afb6288153a27bf48fa1b4362
Habiba Mohamed <107395049+Habiba0079@users.noreply.github.com>
2024-09-28 13:47:16 +0300

    edit style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackList />
+      <Stats />
     </div>
+  );
+}
+
+function Logo() {
+  return <h1>Far Away 🌊</h1>;
+}
+
+function handleSubmit(e)
+{
+  e.preventDefault();
+}
+
+function Form() {
+  return (
+    <form className="add-form" onClick={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((i) => (
+          <option value={i} key={i}>
+            {i}
+          </option>
+        ))}
+      </select>
+
+      <input type="text" placeholder="item..." />
+      <button >Add</button>
+    </form>
+  );
+}
+
+function PackList() {
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {`${item.quantity} ${item.description}`}
+      </span>
+      <button>❌</button>
+    </li>
+  );
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have packed x items</em>
+    </footer>
   );
 }
 
